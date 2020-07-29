086d1d33f9491781fed13514d235c7e8038aa333
@alejandromartinez.0620 <alejandromartinez.0620@gmail.com>
2020-07-29 14:44:48 -0500

    Update with pareto frontier class

--- src/combopt/shortest_paths/__init__.py
+++ src/combopt/shortest_paths/__init__.py
@@ -4,7 +4,8 @@ from .shortest_path_basic import Dijkstra, Reverse_Dijkstra, Bidirectional_Dijks
     spptw_desrochers1988_imp1, spptw_desrochers1988_imp2, spptw_desrochers1988_imp3, spptw_desrochers1988_imp3_bucket, \
     min_time_cost
 
+from .pareto_frontier_structure import Pareto_Frontier
 
-
+from .Shortestpath_resource_const import spptw_desrochers1988_imp_fullpareto
 
 
